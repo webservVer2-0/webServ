25f357e9bdd288a191933ea34b831198e1d1cbcb
jnkeniaem <jeanne.kim9@gmail.com>
2023-03-10 00:07:46 +0900

    FEAT: get method 추가 - 1차

--- src/utils/get_method.cpp
+++ src/utils/get_method.cpp
@@ -17,6 +17,7 @@ t_error ClientGet(struct kevent* event)
 {
 	s_client_type* client = static_cast<s_client_type*>(event->udata);
 	client->SetErrorCode(NO_ERROR);
+	std::vector<struct kevent> tmp;
 	
 	const char* dir; //(request_msg_ 안의 uri).c_str(const char*)
 
@@ -25,14 +26,13 @@ t_error ClientGet(struct kevent* event)
 	if (req_fd == -1)
 	{
 		client->SetErrorCode(SYS_ERR);
-		return (client->GetErrorCode());
+		return client->GetErrorCode();
 	}
 	fcntl(req_fd, F_SETFL, O_NONBLOCK);
 
 	s_base_type* work = client->CreateWork(request uri, req_fd, file);
-	std::vector<struct kevent> tmp;
 
-	ChangeEvents(tmp, req_fd, EVFILT_READ, EV_ADD, 0, ?, work); // ev_disable? ev_add?
+	ChangeEvents(tmp, req_fd, EVFILT_READ, EV_ADD, 0, 0, work); // ev_disable? ev_add?
 	client->SetStage(GET_READY);
 
 	return client->GetErrorCode();
@@ -43,7 +43,22 @@ t_error WorkGet(struct kevent* event)
 	s_work_type* work = static_cast<s_work_type*>(event->udata);
 	// TODO : NO_ERROR로 error setting 필요
 	int	req_fd = work->GetFD();
-	event->
+	size_t file_len = event->data;
+  size_t read_ret = 0;
+	std::vector<struct kevent> tmp;
+
+  work->GetResponseMsg().entity_ = new char[file_len]; // TODO : new error시
+  read_ret = read(req_fd, work->GetResponseMsg().entity_, file_len);
+  if ((read_ret != file_len) || read_ret == -1)
+  {
+    // TODO : work->SetErrorCode(SYS_ERR);
+    // TODO : entity_ 할당해제 여기서?
+    // TODO : return work->GetErrorCode();
+  }
+  close(req_fd);
+
+  ChangeEvents(tmp, work->GetClientPtr()->GetFD(), EVFILT_READ, EV_ADD, 0, 0, work); // work?
+  // TODO : return work->GetErrorCode();
 }
 
 
