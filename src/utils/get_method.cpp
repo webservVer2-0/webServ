bdb2bc8780060335919e38b216f8db05cbbcc31c
jnkeniaem <jeanne.kim9@gmail.com>
2023-03-13 18:54:20 +0900

    WIP : 3/13 모임에서 피드백 받은거 고치는중 - 궁금했던것에대한 답변 작성.(다음 커밋 하기전 삭제할 예정)

--- src/utils/get_method.cpp
+++ src/utils/get_method.cpp
@@ -2,9 +2,9 @@
 
 // TODO : err_custom_ getter, setter 만들까?
 // TODO : error 발생시 err_custom_ 지정해줘야함
-// TODO : CreateWork()시 첫번째 인수 왜 포인터?
-// TODO : s_work_type에서 SetError 필요하지 않을까?
-// TODO : s_work_type client_ptr_ 언제 쓰지? 일단 에러 지정할때만 씀
+// CreateWork()시 첫번째 인수 왜 포인터? > reference로 할까 했는데 const 때문에 복잡해서 std::string*로 설정하심
+// s_work_type에서 SetError 필요하지 않을까? > 괜춘. client에 해주기때문에
+// s_work_type client_ptr_ 언제 쓰지? > 일단 에러 지정하고, kevent 등록할때
 
 /**
  * @brief
@@ -14,7 +14,7 @@
  *
  */
 
-t_error ClientGet(struct kevent* event) {
+void ClientGet(struct kevent* event) {
   s_client_type* client = static_cast<s_client_type*>(event->udata);
   client->SetErrorCode(NO_ERROR);
   const char* dir;  // TODO : (request uri).c_str(const char*)
@@ -22,7 +22,7 @@ t_error ClientGet(struct kevent* event) {
   int req_fd = open(dir, O_RDONLY);
   if (req_fd == -1) {
     client->SetErrorCode(SYS_ERR);
-    // return client->GetErrorCode(); TODO : return?
+    return ;
   }
   fcntl(req_fd, F_SETFL, O_NONBLOCK);
 
@@ -31,10 +31,10 @@ t_error ClientGet(struct kevent* event) {
   ChangeEvents(tmp, req_fd, EVFILT_READ, EV_ADD, 0, 0, work);
   client->SetStage(GET_READY);
 
-  return client->GetErrorCode();
+  return ;
 }
 
-t_error WorkGet(struct kevent* event) {
+void WorkGet(struct kevent* event) {
   s_work_type* work = static_cast<s_work_type*>(event->udata);
   s_client_type* client = static_cast<s_client_type*>(work->GetClientPtr());
   client->SetErrorCode(NO_ERROR);
@@ -48,15 +48,17 @@ t_error WorkGet(struct kevent* event) {
   read_ret = read(req_fd, work->GetResponseMsg().entity_, tmp_entity_len);
   if ((read_ret != tmp_entity_len) || read_ret == -1) {
     client->SetErrorCode(SYS_ERR);
-    // TODO : entity_ 할당해제 여기서?
-    // TODO : return client->GetErrorCode(); ?
+    // entity_ 할당해제 여기서 X
+    return ;
   }
+  // TODO : read 성공시 200으로 바꿔줘야함
+
+  work->ChangeClientEvent(EVFILT_READ, EV_DISABLE, 0, 0, client);
+  // work->ChangeClientEvent(EVFILT_WRITE, EV_ADD, 0, 0, work);
+  // TODO : write enable 해줘야함
+  work->SetClientStage(GET_FIN);
+  // TODO : delete도 해줘야함
   close(req_fd);
 
-  work->ChangeClientEvent(
-      EVFILT_READ, EV_ADD, 0, 0,
-      work);  // TODO : work? < error_code_ 설정안돼있는채로 등록됨
-  work->SetClientStage(GET_FIN);
-
-  return client->GetErrorCode();
-}
+  return ;
+}  // TODO : t_error 반환형말고 void로 해도 됨. 어차피 client에 들어있으니까
