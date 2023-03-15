8754e9d9e0bacc9bb0d00c3946a4608d5ec67c6a
khk811 <hyunkyungkim811@gmail.com>
2023-03-15 20:46:41 +0900

    WIP: POST 기본 틀 작성

--- include/method.hpp
+++ include/method.hpp
@@ -7,17 +7,25 @@
 #include "webserv.hpp"
 
 /**
- * @brief
+ * @brief Request handling 종료 후 실행되는 GET method 입니다.
+ * Request가 끝나고서 서버가 method 요청 처리를 시작하는 지점입니다.
  *
- * @param event
+ * @param event 현재 처리하는 kevent 구조체
  */
 void ClientGet(struct kevent* event);
 
 /**
- * @brief
+ * @brief case Work일때 실행되는 GET method 입니다.
+ * case Client에서 추가된 method 이벤트를 처리합니다.
  *
- * @param event
+ * @param event 현재 처리하는 kevent 구조체
  */
 void WorkGet(struct kevent* event);
+
+// POST (wip)
+void ClientFilePost(struct kevent* event);
+void ClientCGIPost(struct kevent* event);
+void WorkFilePost(struct kevent* event);
+void WorkCGIPost(struct kevent* event);
 
 #endif
--- src/utils/post_method.cpp
+++ src/utils/post_method.cpp
@@ -1,55 +1,81 @@
-// #include "../../include/webserv.hpp"
+#include "../../include/webserv.hpp"
 
-// t_error ClientPost(struct kevent* event) {
-//   s_client_type* client = static_cast<s_client_type*>(event->udata);
-//   t_error error_code = NO_ERROR;
+std::string MakeFileName(s_client_type* client) {
+  std::string upload_path =
+      client->GetConfig().main_config_.find("upload_path")->second;
+  std::string ret = "";
 
-//   // TODO: 저장할 파일 경로가 request_msg 의 어느 부분에 있는지 확인할것.
-//   // 현재는 init_line애 있는지 header에 있는지 알수 없음.
-//   // 일단 작성은 init_line으로 하고 있다고 간주함.
-//   //   const char* dir =
-//   // client->GetRequest().init_line_.find("uri")->second.c_str();
-//   char* dir = "./index.html";
+  ret.append(upload_path);
+  ret.append(client->GetCookieId());
+  ret.push_back('_');
+  ret.append("userdata.dat");
+  return ret;
+}
 
-//   int save_file_fd = open(dir, O_RDWR | O_CREAT);
-//   if (save_file_fd == -1) {
-//     // error;
-//     // error_code = SYS_ERR;
-//     // return error_code;
-//   }
-//   std::vector<struct kevent> tmp;
-//   s_base_type* new_work =
-//       client->CreateWork(&(std::string(dir)), save_file_fd, file);
-//   ChangeEvents(tmp, client->GetFD(), EVFILT_READ, EV_DISABLE, 0, 0, client);
-//   ChangeEvents(tmp, save_file_fd, EVFILT_READ, EV_ADD, 0, 0, new_work);
-//   client->SetStage(POST_READY);
+const char* MakeFileDirectory(std::string& uri) {
+  const char* dir = uri.c_str();
 
-//   // TODO: return 값을 void로 수정하고, client->GetErrorCode()만 바꿔주기;
-//   return error_code;  // return client->GetErrorCode();
-// }
+  while (IsExistFile(dir)) {
+    // TODO: file name change
+  }
+  return dir;
+}
 
-// t_error WorkPost(struct kevent* event) {
-//   s_work_type* to_do = static_cast<s_work_type*>(event->udata);
-//   s_client_type* client = static_cast<s_client_type*>(to_do->GetClientPtr());
-//   t_error error_code = NO_ERROR;
-//   size_t write_result = 0;
+void ClientFilePost(struct kevent* event) {
+  s_client_type* client = static_cast<s_client_type*>(event->udata);
+  client->SetErrorCode(NO_ERROR);
 
-//   write_result = write(to_do->GetFD(), client->GetResponse().entity_,
-//                        client->GetRequest().entity_length_);
-//   if (write_result != client->GetRequest().entity_length_) {
-//     // error;
-//     // error_code = SYS_ERR;
-//     // return error_code;
-//   }
-//   std::vector<struct kevent> tmp;
+  std::string uri = MakeFileName(client);
+  // TODO: 먼저 file exist 검사 (반복문 검사);
+  const char* dir = MakeFileDirectory(uri);
 
-//   ChangeEvents(tmp, to_do->GetFD(), EV_DELETE, EV_ADD | EV_EOF, 0, 0, NULL);
-//   client->SetStage(
-//       POST_FIN);  // TODO: POST 파일 저장 후 검사가 필요한지 확인하기
-//   to_do->ChangeClientEvent(EVFILT_WRITE, EV_ENABLE, 0, 0, client);
-//   close(to_do->GetFD());
+  int save_file_fd = open(dir, O_RDWR | O_CREAT);
+  if (save_file_fd == -1) {
+    client->SetErrorCode(SYS_ERR);
+    client->SetStage(ERR_FIN);
+    return;
+  }
+  std::vector<struct kevent> tmp;
+  s_base_type* new_work = client->CreateWork(&(uri), save_file_fd, file);
+  ChangeEvents(tmp, client->GetFD(), EVFILT_READ, EV_DISABLE, 0, 0, client);
+  ChangeEvents(tmp, save_file_fd, EVFILT_READ, EV_ADD, 0, 0, new_work);
+  client->SetErrorCode(OK);
+  client->SetStage(POST_START);
 
-//   // TODO: 캐시페이지 추가?
+  return;
+}
 
-//   return error_code;
-// }
+void ClientCGIPost(struct kevent* event) {
+  (void)event;
+  return;
+}
+
+void WorkFilePost(struct kevent* event) {
+  s_work_type* to_do = static_cast<s_work_type*>(event->udata);
+  s_client_type* client = static_cast<s_client_type*>(to_do->GetClientPtr());
+  client->SetErrorCode(NO_ERROR);
+  size_t write_result = 0;
+
+  write_result = write(to_do->GetFD(), client->GetResponse().entity_,
+                       client->GetRequest().entity_length_);
+  if (write_result != client->GetRequest().entity_length_) {
+    client->SetErrorCode(SYS_ERR);
+    client->SetStage(ERR_FIN);
+    return;
+  }
+
+  std::vector<struct kevent> tmp;
+  ChangeEvents(tmp, to_do->GetFD(), EV_DELETE, EV_ADD | EV_EOF, 0, 0, NULL);
+  to_do->ChangeClientEvent(EVFILT_WRITE, EV_ENABLE, 0, 0, client);
+  close(to_do->GetFD());
+  client->SetErrorCode(OK);
+  client->SetStage(POST_FIN);
+  // TODO: POST 파일 저장 후 검사가 필요한지 확인하기
+
+  return;
+}
+
+void WorkCGIPost(struct kevent* event) {
+  (void)event;
+  return;
+}
