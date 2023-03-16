04d1a37b857da8d9a763757c857744a11e90ba53
khk811 <hyunkyungkim811@gmail.com>
2023-03-16 21:42:59 +0900

    CHORE: method 함수 doxygen 작성

--- include/method.hpp
+++ include/method.hpp
@@ -22,10 +22,36 @@ void ClientGet(struct kevent* event);
  */
 void WorkGet(struct kevent* event);
 
-// POST (wip)
+/**
+ * @brief Request handling 종료 후 실행되는 POST method 입니다.
+ * 이 함수는 POST 요청이 '파일'인 경우에 호출됩니다.
+ *
+ * @param event 현재 처리하는 kevent 구조체
+ */
 void ClientFilePost(struct kevent* event);
+
+/**
+ * @brief Request handling 종료 후 실행되는 POST method 입니다.
+ * 이 함수는 POST 요청이 'CGI'인 경우에 호출됩니다.
+ *
+ * @param event 현재 처리하는 kevent 구조체
+ */
 void ClientCGIPost(struct kevent* event);
+
+/**
+ * @brief case Work일때 실행되는 POST method 입니다.
+ * 이 함수는 POST 요청이 '파일'인 경우에 호출됩니다.
+ *
+ * @param event 현재 처리하는 kevent 구조체
+ */
 void WorkFilePost(struct kevent* event);
+
+/**
+ * @brief case Work일때 실행되는 POST method 입니다.
+ * 이 함수는 POST 요청이 'CGI'인 경우에 호출됩니다.
+ *
+ * @param event 현재 처리하는 kevent 구조체
+ */
 void WorkCGIPost(struct kevent* event);
 
 #endif
