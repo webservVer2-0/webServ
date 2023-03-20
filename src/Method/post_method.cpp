acb4612df82ac38791c210b1535060c8952c573e
khk811 <hyunkyungkim811@gmail.com>
2023-03-20 15:11:39 +0900

    FIX: write 결과에 따라 추가읽기하게 수정

--- src/Method/post_method.cpp
+++ src/Method/post_method.cpp
@@ -82,7 +82,7 @@ void ClientFilePost(struct kevent* event) {
   std::string file_path = MakeFilePath(client);
   const char* path_char = AppendNumSuffix(file_path);
 
-  int save_file_fd = open(path_char, O_RDWR | O_CREAT | O_NONBLOCK);
+  int save_file_fd = open(path_char, O_RDWR | O_CREAT | O_APPEND | O_NONBLOCK);
   if (save_file_fd == -1) {
     client->SetError(errno, "POST method open()");
     client->SetErrorCode(SYS_ERR);
@@ -109,31 +109,37 @@ void ClientCGIPost(struct kevent* event) {
 void WorkFilePost(struct kevent* event) {
   s_work_type* work = static_cast<s_work_type*>(event->udata);
   s_client_type* client = static_cast<s_client_type*>(work->GetClientPtr());
-  size_t write_result = 0;
 
-  write_result = write(work->GetFD(), client->GetRequest().entity_,
-                       client->GetRequest().entity_length_);
-  if (write_result != client->GetRequest().entity_length_) {
+  size_t write_result = write(work->GetFD(), client->GetRequest().entity_,
+                              client->GetRequest().entity_length_);
+  size_t total_write_len = client->GetMessageLength() + write_result;
+  size_t entity_len = client->GetRequest().entity_length_;
+
+  if (total_write_len > entity_len) {
     client->SetError(errno, "POST method write()");
     client->SetErrorCode(SYS_ERR);
     client->SetStage(ERR_FIN);
     return;
-  }
-
-  ServerConfig::ChangeEvents(work->GetFD(), EVFILT_WRITE, EV_DELETE, 0, 0,
-                             NULL);
-  work->ChangeClientEvent(EVFILT_WRITE, EV_ENABLE, 0, 0, client);
-  if (close(work->GetFD()) == -1) {
-    client->SetError(errno, "POST method close()");
-    client->SetErrorCode(SYS_ERR);
-    client->SetStage(ERR_FIN);
+  } else if (total_write_len < entity_len) {
+    if (write_result != -1) client->SetMessageLength(total_write_len);
     return;
+  } else {
+    ServerConfig::ChangeEvents(work->GetFD(), EVFILT_WRITE, EV_DELETE, 0, 0,
+                               NULL);
+    work->ChangeClientEvent(EVFILT_WRITE, EV_ENABLE, 0, 0, client);
+    if (close(work->GetFD()) == -1) {
+      client->SetError(errno, "POST method close()");
+      client->SetErrorCode(SYS_ERR);
+      client->SetStage(ERR_FIN);
+      return;
+    }
+    client->SetMimeType(work->GetUri());
+    client->SetErrorCode(OK);
+    client->SetStage(POST_FIN);
+    client->SetMessageLength(0);
   }
-  client->SetMimeType(work->GetUri());
-  client->SetErrorCode(OK);
-  client->SetStage(POST_FIN);
   // TODO: POST 파일 저장 후 검사가 필요한지 확인하기
-  // TODO: DeleteUdata()로 work에서 사용이 끝난 udata 삭제
+  // TODO: work에서 사용이 끝난 udata를 리셋
   return;
 }
 
