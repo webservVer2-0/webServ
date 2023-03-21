51cedb090707398d82ef29680bf1d7c9908022e4
jnkeniaem <jeanne.kim9@gmail.com>
2023-03-21 18:25:04 +0900

    TEST : new char[0] > new char[BUF_SIZE] >> error solved - 이 상황에선 0보다 크게 할당해줘야하나봐

--- test/jeekim/get_read_test.cpp
+++ test/jeekim/get_read_test.cpp
@@ -1,15 +1,74 @@
-#include <fcntl.h>
-#include <stdlib.h>
-#include <iostream>
-#include <unistd.h>
-#include <vector>
-#include <sys/stat.h>
+#include "get_read_test.hpp"
 
-#define BUF_SIZE 3
-// #define BUF_SIZE 1024
+void for_read(struct ke* kev)
+{
+	s_client_type* client = static_cast<s_client_type*>(kev->udat);
+	int	read_ret = read(client->fd, client->buf, BUF_SIZE);
+	if (read_ret == -1 || read_ret)
+	{
+		return ;
+	}
+	for (int i = 0; i < read_ret; i++)
+	{
+		client->vec.push_back(client->buf[i]);
+	}
+	//TODO : buf content 다 0 대입
+	std::cout << "read_ret : " << read_ret << std::endl;
+	if (client->vec.size() != client->len)
+	{
+		std::cout << "client->vec.size() != len\n";
+		exit(1);
+	}
+	int	idx = 0;
+	std::vector<char>::iterator it;
+	for (it = client->vec.begin(); it != client->vec.end(); it++)
+	{
+		client->buf[idx++] = *it;
+	}
+	if (strlen(client->buf) != client->len)
+	{
+		std::cout << "buf 길이 != len\n";
+		exit(1);
+	}
+	std::cout << "buf : " << client->buf << std::endl;
+	client->type = 3;
+	return ;
+}
 
 int main()
 {
+	struct ke* kev = new (ke);
+	kev->udat = new (s_client_type);
+	s_client_type* client = static_cast<s_client_type*>(kev->udat);
+	client->len = (size_t)11;
+	client->fd = open("a.txt", O_RDONLY);
+	if (client->fd == -1) std::cout << "open() error\n";
+	client->type = 2;
+	try
+	{	client->buf = new char[BUF_SIZE];	} // () : for value-initialize
+	// https://stackoverflow.com/questions/2204176/how-to-initialise-memory-with-new-operator-in-c
+	catch (const std::exception& e) {std::cout << "new error()";}
+	kev->udat = static_cast<void*>(client);
+	while (1)
+	{
+		if (client->type == 2)
+			for_read(kev);
+		else if (client->type == 3)
+			break;
+	}
+	// a->fd = open("screenshot.png", O_RDONLY);
+	// int	fd2 = open("screenshot2.png", O_CREAT | O_WRONLY | O_APPEND, 0644);
+	// if (fd2 == -1) std::cout << "open2() error\n";
+		// if (write(fd2, entity, read_ret) == -1) std::cout << "write() error\n";
+
+	
+	// std::cout << "buf : " << buf << std::endl;
+		// printf("%s\n", buf);
+	// TODO : buf_size < entity_len
+	// for (int i = 0; i < vec.size(); i++)
+	// {	entity[i] = vec.at(i);	}
+	//TODO : 위험 요소 더 알아보기
+	/*
 	struct stat file_info;
 	int ret;
 
@@ -19,52 +78,7 @@ int main()
 		return (-1);
 	}
 	file_info.st_size;
-	int	fd = open("screenshot.png", O_RDONLY);
-	if (fd == -1) std::cout << "open() error\n";
-	int	fd2 = open("screenshot2.png", O_CREAT | O_WRONLY | O_APPEND, 0644);
-	if (fd2 == -1) std::cout << "open2() error\n";
-	std::vector<char> vec;
-	std::vector<char>::iterator it;
-	char* buf;
-	try
-	{	buf = new char[0];	} // () : for value-initialize
-	// https://stackoverflow.com/questions/2204176/how-to-initialise-memory-with-new-operator-in-c
-	catch (const std::exception& e) {std::cout << "new error()";}
-	char* entity;
-	try
-	{	entity = new char[BUF_SIZE]();	}
-	catch (const std::exception& e) {std::cout << "new2 error()";}
-
-	int	read_ret = 0;
-	while ((read_ret = read(fd, buf, BUF_SIZE)) && (read_ret != -1))//! : while(read()) > 여기서 막히기 때문에 안됨.
-	{
-		for (int i = 0; i < read_ret; i++)//read_ret == -1 이면 위험
-		{
-			vec.push_back(buf[i]);
-			// entity[i] = vec.at(i);
-		}
-		// if (write(fd2, entity, read_ret) == -1) std::cout << "write() error\n";
-		// delete[] entity;
-	}
-	//TODO : buf content 다 0 대입
-	if (read_ret == -1)	std::cout << "read() error\n";
-	int	idx = 0;
-	for (it = vec.begin(); it != vec.end(); it++)
-	{
-		buf[idx++] = *it;
-		// std::cout << *it;
-	}
-	for (int idx = 0; buf[idx]; idx++)
-		;
-	if (write(fd2, buf, idx) == -1) std::cout << "write() error\n";
-	
-	// std::cout << "buf : " << buf << std::endl;
-		// printf("%s\n", buf);
-	// TODO : buf_size < entity_len
-	// for (int i = 0; i < vec.size(); i++)
-	// {	entity[i] = vec.at(i);	}
-	// if (write(fd2, entity, BUF_SIZE) == -1) std::cout << "write() error\n";
-//TODO : 위험 요소 더 알아보기
+	*/
 }
 
 /* 이상해이상해코드
