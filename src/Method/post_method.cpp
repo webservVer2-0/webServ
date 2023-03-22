aed39824ea6ac3adeb33408657f1af7f5ff1dce8
khk811 <hyunkyungkim811@gmail.com>
2023-03-22 14:42:28 +0900

    CHORE: Custom error msg 수정

--- src/Method/post_method.cpp
+++ src/Method/post_method.cpp
@@ -84,7 +84,7 @@ void ClientFilePost(struct kevent* event) {
 
   int save_file_fd = open(path_char, O_RDWR | O_CREAT | O_APPEND | O_NONBLOCK);
   if (save_file_fd == -1) {
-    client->SetErrorString(errno, "POST method open()");
+    client->SetErrorString(errno, "post_method.cpp / open()");
     client->SetErrorCode(SYS_ERR);
     client->SetStage(ERR_FIN);
     return;
@@ -117,7 +117,7 @@ void WorkFilePost(struct kevent* event) {
   size_t entity_len = client->GetRequest().entity_length_;
 
   if (total_write_len > entity_len) {
-    client->SetErrorString(errno, "POST method write()");
+    client->SetErrorString(errno, "post_method.cpp / write()");
     client->SetErrorCode(SYS_ERR);
     client->SetStage(ERR_FIN);
     return;
@@ -129,7 +129,7 @@ void WorkFilePost(struct kevent* event) {
                                NULL);
     work->ChangeClientEvent(EVFILT_WRITE, EV_ENABLE, 0, 0, client);
     if (close(work->GetFD()) == -1) {
-      client->SetErrorString(errno, "POST method close()");
+      client->SetErrorString(errno, "post_method.cpp / close()");
       client->SetErrorCode(SYS_ERR);
       client->SetStage(ERR_FIN);
       return;
