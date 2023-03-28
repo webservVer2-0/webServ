469abe60586594d4120d1a06f8324cd6e1a3fc2a
khk811 <hyunkyungkim811@gmail.com>
2023-03-28 15:50:23 +0900

    FEAT: DELETE 함수 추가(Request entity 사용)

--- src/Method/delete_method.cpp
+++ src/Method/delete_method.cpp
@@ -1,27 +1,37 @@
 #include "../../include/webserv.hpp"
 
 /**
- * @brief Request에서 변경한 URI에서 "?delete_target=" 문자열을
- * 제거한 문자열(삭제 파일 경로)을 생성하는 함수입니다.
+ * @brief Request entity로 들어오는 "?delete_target=..." 쿼리문에서
+ * delete_target을 분리한 후, upload_path를 붙여 지울 파일의 경로를 std::string
+ * 형태로 생성하는 함수입니다.
  *
- * @param uri_w_query Query("?delete_target=")가 들어있는 URI
- * @return std::string Query가 삭제된 URI string (삭제 파일 경로)
- * @note POST에 있는 파일 경로 함수와 다르게 이 함수는 인자로 std::string&가
- * 아닌 std::string을 받습니다. 그 결과 client 내부의 ConvertedURI는
- * 변경되지 않습니다.
+ * @param client s_client_type의 udata
+ * @return std::string 실제 지울 delete_target이 존재하는 파일 경로
  */
-std::string EraseQueryFromUri(std::string uri_w_query) {
-  size_t query_start = uri_w_query.find("?");
-  size_t erase_len = (uri_w_query.find("=") - query_start) + 1;
+std::string MakeDeleteTargetPath(s_client_type* client) {
+  char* raw_entity = client->GetRequest().entity_;
+  size_t entity_len = client->GetRequest().entity_length_;
 
-  return uri_w_query.erase(query_start, erase_len);
+  char* target_start = strchr(raw_entity, '=') + 1;
+  size_t target_len = (raw_entity + entity_len) - target_start;
+  char* delete_target = new char[target_len + 1];
+
+  memmove(delete_target, target_start, target_len);
+  delete_target[target_len] = '\0';
+
+  std::string delete_path(delete_target);
+  delete[] delete_target;
+  delete_path.insert(0, "/");
+  delete_path.insert(
+      0, client->GetConfig().main_config_.find("upload_path")->second);
+  return delete_path;
 }
 
 void ClientDelete(struct kevent* event) {
   s_client_type* client = static_cast<s_client_type*>(event->udata);
 
-  std::string file_path = EraseQueryFromUri(client->GetConvertedURI());
-  const char* path_char = file_path.c_str();
+  std::string delete_path = MakeDeleteTargetPath(client);
+  const char* path_char = delete_path.c_str();
 
   unlink(path_char);
 
